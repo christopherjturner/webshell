f03471f306eb383e81ef4b1b7573db9d582921a5
Ben Chidgey <ben@feedmypixel.com>
2024-10-18 15:13:55 +0100

    Make web shell full screen
    - Formatting
    - Some html tags
    - Updates to js
    - Updates to styles

--- assets/main.js
+++ assets/main.js
@@ -1,60 +1,70 @@
-var terminal
+let terminal
+
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => func.apply(this, args), timeout)
+    }
+}
 
 function init(shellPath) {
-  terminal = new Terminal({
-    screenKeys: true,
-    useStyle: true,
-    cursorBlink: true,
-    fullscreenWin: true,
-    maximizeWin: true,
-    screenReaderMode: true,
-    cols: 128,
-    fontFamily: 'Terminal, monospace'
-  })
-
-
-  var protocol = (location.protocol === "https:") ? "wss://" : "ws://"
-  var url = protocol + location.host + shellPath
-  var ws = new WebSocket(url)
-  var attachAddon = new AttachAddon.AttachAddon(ws)
-  var fitAddon = new FitAddon.FitAddon()
-
-  terminal.loadAddon(attachAddon)
-  terminal.loadAddon(fitAddon)
-  terminal.open(document.getElementById("terminal"))
-  terminal._initialized = true
-
-    function ping() {
-      if (ws && ws.readyState === 1) {
-          var msg = new TextEncoder().encode("\x01PING" )
-          ws.send(msg);
-      }
-      setTimeout(ping, 1000);
-    }
-
-  ws.onclose = function(event) {
-    terminal.write('\r\n\nconnection has been closed\n')
-  }
-
-  ws.onopen = function() {
-
-    terminal.focus()
-    setTimeout(function() {fitAddon.fit()})
-
-    terminal.onResize(function(event) {
-      var rows = event.rows
-      var cols = event.cols
-      var resize = JSON.stringify({cols: cols, rows: rows + 1})
-      var msg = new TextEncoder().encode("\x01" + resize)
-      ws.send(msg)
+    terminal = new Terminal({
+        screenKeys: true,
+        useStyle: true,
+        cursorBlink: true,
+        fullscreenWin: true,
+        maximizeWin: true,
+        screenReaderMode: true,
+        fontFamily: 'Terminal, monospace',
+        scrollOnUserInput: false
     })
 
-    ping()
 
-    window.onresize = function() {
-      fitAddon.fit()
+    const protocol = (location.protocol === "https:") ? "wss://" : "ws://"
+    const url = protocol + location.host + shellPath
+    const ws = new WebSocket(url)
+    const attachAddon = new AttachAddon.AttachAddon(ws)
+    const fitAddon = new FitAddon.FitAddon()
+
+    terminal.loadAddon(attachAddon)
+    terminal.loadAddon(fitAddon)
+    terminal.open(document.getElementById("terminal"))
+    terminal._initialized = true
+
+    function ping() {
+        if (ws && ws.readyState === 1) {
+            const msg = new TextEncoder().encode("\x01PING")
+            ws.send(msg);
+        }
+        setTimeout(ping, 1000);
     }
-  }
+
+    ws.onclose = function () {
+        terminal.write('\r\n\nconnection has been closed\n')
+    }
+
+    ws.onopen = function () {
+        terminal.focus()
+        setTimeout(function () {
+            fitAddon.fit()
+        })
+
+        terminal.onResize(debounce(function (event) {
+            const rows = event.rows
+            const cols = event.cols
+            const resize = JSON.stringify({ cols: cols, rows: rows + 1 })
+            console.log(resize)
+            const msg = new TextEncoder().encode("\x01" + resize)
+            ws.send(msg)
+        }))
+
+        ping()
+
+        window.onresize = debounce(function () {
+            fitAddon.fit()
+        })
+    }
 }
 
 
